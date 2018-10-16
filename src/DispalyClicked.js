21f49f75d38034d0020c4b477e1f603934c96f3c
Paul Standley <paulstandley1972@gmail.com>
2018-10-17 00:13:51 +0100

    display one or two images

--- src/DispalyClicked.js
+++ src/DispalyClicked.js
@@ -12,15 +12,16 @@ class DispalyClicked extends Component {
     if(this.props.listInfo.pick.response.photos.items[0] !== undefined) {
     console.log(this.props.listInfo.pick.response.photos.items[0]);
     console.log(this.props.listInfo.pick.response.photos.items[1]);
-    
-    let prefix0 = this.props.listInfo.pick.response.photos.items[0].prefix;
-    let suffix0 = this.props.listInfo.pick.response.photos.items[0].suffix;
-    let size0 =  this.props.listInfo.pick.response.photos.items[0].width;
+      var prefix0 = this.props.listInfo.pick.response.photos.items[0].prefix;
+      var suffix0 = this.props.listInfo.pick.response.photos.items[0].suffix;
+      var size0 =  this.props.listInfo.pick.response.photos.items[0].width;
 
-    let suffix1 = this.props.listInfo.pick.response.photos.items[1].suffix;
-    let prefix1 = this.props.listInfo.pick.response.photos.items[1].prefix;
-    let size1 =  this.props.listInfo.pick.response.photos.items[1].width;
-    
+    }
+    if(this.props.listInfo.pick.response.photos.items[1] !== undefined) {
+      var suffix1 = this.props.listInfo.pick.response.photos.items[1].suffix;
+      var prefix1 = this.props.listInfo.pick.response.photos.items[1].prefix1;
+      var size1 =  this.props.listInfo.pick.response.photos.items[1].width;
+    }
 
 /*
     <img src={`${prefix0}${suffix0}`} alt="Smiley face" height="42" width="42"/>
@@ -32,16 +33,26 @@ class DispalyClicked extends Component {
    /*  look at this tommorow  */
     return ( 
       <div className="dispalyClicked">
-        <h2>{this.props.listInfo.AppData.listActiveTargetName}</h2>
-        <img src={`${prefix0}${size0}${suffix0}`} alt={this.props.listInfo.AppData.listActiveTargetName} height="420" width="420"/>
-        <h2>{this.props.listInfo.AppData.listActiveTargetAddress}</h2>
-        <img src={`${prefix1}${size1}${suffix1}`} alt={this.props.listInfo.AppData.listActiveTargetName} height="420" width="420"/>
+        {this.props.listInfo.pick.response.photos.items[0] !== undefined ? 
+          <span className="topList">
+          <h2>{this.props.listInfo.AppData.listActiveTargetName}</h2>
+          <img src={`${prefix0}${size0}${suffix0}`} alt={this.props.listInfo.AppData.listActiveTargetName} height="420" width="420"/>
+        </span>
+          : '' }
+        {this.props.listInfo.pick.response.photos.items[1] !== undefined ?
+          <span className="bottomList">
+          <h2>{this.props.listInfo.AppData.listActiveTargetAddress}</h2>
+          <img src={`${prefix1}${size1}${suffix1}`} alt={this.props.listInfo.AppData.listActiveTargetName} height="420" width="420"/>
+        </span>  
+          : '' }
+        
         <button onClick={this.props.listInfo.closeList} >close</button>
       </div>
      );
     }
-  }
-    return ('');
+  
+
+    return (<button onClick={this.props.listInfo.closeList} >Soz do info</button>);
 
   }
 }
